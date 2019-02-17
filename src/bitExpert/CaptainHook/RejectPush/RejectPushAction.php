acf76719154297e333c467379c8df403ce13ee02
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2019-02-17 11:32:40 +0100

    Add RejectPushAction

---
+++ src/bitExpert/CaptainHook/RejectPush/RejectPushAction.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the Captain Hook Reject Push plugin package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace bitExpert\CaptainHook\RejectPush;
+
+use CaptainHook\App\Config;
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Hook\Action;
+use SebastianFeldmann\Cli\Command\Runner\Simple;
+use SebastianFeldmann\Cli\Processor\ProcOpen as Processor;
+use SebastianFeldmann\Git\Command\Log\Commits;
+use SebastianFeldmann\Git\Command\Log\Commits\Jsonized;
+use SebastianFeldmann\Git\Repository;
+
+class RejectPushAction implements Action
+{
+    /**
+     * @var Simple
+     */
+    private $runner;
+
+    /**
+     * Creates new {@link \bitExpert\CaptainHook\RejectPush\RejectPushAction}.
+     */
+    public function __construct()
+    {
+        $this->runner = new Simple(new Processor());
+    }
+
+    /**
+     * Executes the action.
+     *
+     * @param  \CaptainHook\App\Config $config
+     * @param  \CaptainHook\App\Console\IO $io
+     * @param  \SebastianFeldmann\Git\Repository $repository
+     * @param  \CaptainHook\App\Config\Action $action
+     * @return void
+     * @throws \Exception
+     */
+    public function execute(Config $config, IO $io, Repository $repository, Config\Action $action): void
+    {
+        $arguments = $io->getArguments();
+        $target = $arguments['target'] ?? '';
+        if (empty($target)) {
+            return;
+        }
+
+        $options = $action->getOptions()->getAll();
+        $notAllowedCommits = $options[$target] ?? [];
+        if(!is_array($notAllowedCommits)) {
+            $notAllowedCommits = [$notAllowedCommits];
+        }
+
+        if(count($notAllowedCommits) === 0) {
+            // no need to iterate over the whole Git history when no configuration for the $target
+            // was found
+            return;
+        }
+
+        foreach ($this->getAllCommits($repository) as $commit) {
+            /** @var \SebastianFeldmann\Git\Log\Commit $commit */
+            if (in_array($commit->getHash(), $notAllowedCommits, true)) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Commit "%s" found! Not able to push to "%s"!',
+                        $commit->getHash(),
+                        $target
+                    )
+                );
+            }
+        }
+    }
+
+    /**
+     * Returns a list of all commits of the current branch.
+     *
+     * @param Repository $repository
+     * @return iterable
+     */
+    protected function getAllCommits(Repository $repository): iterable
+    {
+        $cmd = (new Commits($repository->getRoot()))
+            ->prettyFormat(Jsonized::FORMAT);
+
+        $result = $this->runner->run($cmd, new Jsonized());
+        return $result->getFormattedOutput();
+    }
+}
---
+++ tests/bitExpert/CaptainHook/RejectPush/RejectPushActionUnitTest.php
@@ -0,0 +1,212 @@
+<?php
+
+/*
+ * This file is part of the Captain Hook Reject Push plugin package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use bitExpert\CaptainHook\RejectPush\RejectPushAction;
+use CaptainHook\App\Config;
+use CaptainHook\App\Config\Action;
+use CaptainHook\App\Config\Options;
+use CaptainHook\App\Console\IO;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use SebastianFeldmann\Git\Log\Commit;
+use SebastianFeldmann\Git\Repository;
+
+class RejectPushActionUnitTest extends TestCase
+{
+    /**
+     * @var Config|MockObject
+     */
+    private $config;
+    /**
+     * @var IO|MockObject
+     */
+    private $io;
+    /**
+     * @var Repository|MockObject
+     */
+    private $repository;
+    /**
+     * @var Action|MockObject
+     */
+    private $action;
+    /**
+     * @var RejectPushAction
+     */
+    private $hook;
+
+    /**
+     * {@inheritDoc}
+     * @throws ReflectionException
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->config = $this->createMock(Config::class);
+        $this->io = $this->createMock(IO::class);
+        $this->repository = $this->createMock(Repository::class);
+        $this->action = $this->createMock(Action::class);
+        $this->hook = $this->createPartialMock(RejectPushAction::class, ['getAllCommits']);
+    }
+
+    /**
+     * @test
+     */
+    public function missingTargetIOParameterStopsExecution()
+    {
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn([]);
+
+        $this->action->expects(self::never())
+            ->method('getOptions');
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function missingConfigurationForTargetStopsExecution()
+    {
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options([]));
+
+        $this->hook->expects(self::never())
+            ->method('getAllCommits');
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function emptyConfigurationArrayForTargetStopsExecution()
+    {
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options(['my-origin' => []]));
+
+        $this->hook->expects(self::never())
+            ->method('getAllCommits');
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function allowPushWhenConfiguredCommitIdsCannotBeFoundInCommitHistory()
+    {
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options(['my-origin' => ['abcdefg', 'bcdefgh']]));
+
+        $this->hook->expects(self::once())
+            ->method('getAllCommits')
+            ->willReturn($this->createCommitHistory());
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function allowPushWhenConfiguredCommitIdCannotBeFoundInCommitHistory()
+    {
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options(['my-origin' => 'abcdefg']));
+
+        $this->hook->expects(self::once())
+            ->method('getAllCommits')
+            ->willReturn($this->createCommitHistory());
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function denyPushWhenConfiguredCommitIdsAreFoundInCommitHistory()
+    {
+        self::expectException(\RuntimeException::class);
+
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options(['my-origin' => ['2345678', '3456789']]));
+
+        $this->hook->expects(self::once())
+            ->method('getAllCommits')
+            ->willReturn($this->createCommitHistory());
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * @test
+     */
+    public function denyPushWhenConfiguredCommitIdIsFoundInCommitHistory()
+    {
+        self::expectException(\RuntimeException::class);
+
+        $this->io->expects(self::once())
+            ->method('getArguments')
+            ->willReturn(['target' => 'my-origin']);
+
+        $this->action->expects(self::once())
+            ->method('getOptions')
+            ->willReturn(new Options(['my-origin' => '1234567']));
+
+        $this->hook->expects(self::once())
+            ->method('getAllCommits')
+            ->willReturn($this->createCommitHistory());
+
+        $this->hook->execute($this->config, $this->io, $this->repository, $this->action);
+    }
+
+    /**
+     * Helper method to return a default Git Commit History collection
+     *
+     * @throws Exception
+     */
+    private function createCommitHistory(): iterable
+    {
+        return [
+            new Commit('1234567', [], '', new DateTimeImmutable(), 'Author'),
+            new Commit('2345678', [], '', new DateTimeImmutable(), 'Author'),
+            new Commit('3456789', [], '', new DateTimeImmutable(), 'Author'),
+            new Commit('4567890', [], '', new DateTimeImmutable(), 'Author'),
+
+        ];
+    }
+}
